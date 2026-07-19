357080c31ce804d6879b0add6c9e35891652e09e
ayutripathi2 <tripathiayu15@gmail.com>
2026-07-20 03:31:28 +0530

    Fix Railway DB port

--- databse.py
+++ databse.py
@@ -8,6 +8,7 @@ import mysql.connector
 
 connection = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
     database=os.getenv("DB_NAME")
